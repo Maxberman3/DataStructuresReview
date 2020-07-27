8fae189675c5f84874e5aa4f62ba14135bb6ad41
Berman, Maxwell <maxwell.berman@emerson.com>
2020-07-27 15:16:35 -0400

    Started implementing remove

--- MaxDataStructures/MaxDataStructures/BinarySearchTree.cs
+++ MaxDataStructures/MaxDataStructures/BinarySearchTree.cs
@@ -2,6 +2,28 @@
 {
     public class BinarySearchTree
     {
+        private BinarySearchTreeNode Root { get; set; }
+
+        public void Insert(int value)
+        {
+            if (Root == null)
+            {
+                Root = new BinarySearchTreeNode(value);
+            }
+            else
+            {
+                Root.Insert(value);
+            }
+        }
+        public bool Search(int value)
+        {
+            if (Root == null)
+            {
+                return false;
+            }
+            return Root.Search(value);
+        }
+
 
 
 
@@ -15,9 +37,77 @@
             public BinarySearchTreeNode Left { get; set; }
             public BinarySearchTreeNode Right { get; set; }
 
-            public BinarySearchTreeNode(int value)
+            public BinarySearchTreeNode(int val)
             {
-                Value = value;
+                Value = val;
+            }
+            public void Insert(int val)
+            {
+                if (val > Value)
+                {
+                    if (Right == null)
+                    {
+                        Right = new BinarySearchTreeNode(val);
+                    }
+                    else
+                    {
+                        Right.Insert(val);
+                    }
+                }
+                else
+                {
+                    if (Left == null)
+                    {
+                        Left = new BinarySearchTreeNode(val);
+                    }
+                    else
+                    {
+                        Left.Insert(val);
+                    }
+                }
+            }
+            public bool Search(int val)
+            {
+                if (Value == val)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (Left != null)
+                    {
+                        if (Left.Search(val))
+                        {
+                            return true;
+                        }
+                    }
+                    if (Right != null)
+                    {
+                        if (Right.Search(val))
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+            public void Remove(int val)
+            {
+                if (Value == val)
+                {
+                    if (Right.IsLeaf())
+                    {
+
+                    }
+                }
+            }
+            private bool IsLeaf()
+            {
+                if (Left == null && Right == null)
+                {
+                    return true;
+                }
+                return false;
             }
         }
     }
