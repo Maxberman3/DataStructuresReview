df2122ad2e3fbfb0edcc8c710606dc1835e1fe4c
Berman, Maxwell <maxwell.berman@emerson.com>
2020-08-05 14:48:49 -0400

    Finished the remove

--- MaxDataStructures/MaxDataStructures/BinarySearchTree.cs
+++ MaxDataStructures/MaxDataStructures/BinarySearchTree.cs
@@ -24,7 +24,38 @@
             return Root.Search(value);
         }
 
-
+        public void Remove(int value)
+        {
+            if (value == Root.Value)
+            {
+                if (Root.IsLeaf())
+                {
+                    Root = null;
+                }
+                else if (Root.Right == null && Root.Left != null)
+                {
+                    Root = Root.Left;
+                }
+                else if (Root.Right != null && Root.Left == null)
+                {
+                    Root = Root.Right;
+                }
+                else
+                {
+                    int temp = Root.Right.Value;
+                    Root.Right.Remove(temp, Root);
+                    Root.Value = temp;
+                }
+            }
+            else if (value > Root.Value)
+            {
+                Root.Right.Remove(value, Root);
+            }
+            else
+            {
+                Root.Left.Remove(value, Root);
+            }
+        }
 
 
 
@@ -91,17 +122,71 @@
                     return false;
                 }
             }
-            public void Remove(int val)
+            public void Remove(int val, BinarySearchTreeNode Previous)
             {
-                if (Value == val)
+                bool wentLeft = Previous.Value >= Value;
+                if (val == Value)
                 {
-                    if (Right.IsLeaf())
+                    if (IsLeaf())
                     {
-
+                        if (wentLeft)
+                        {
+                            Previous.Left = null;
+                        }
+                        else
+                        {
+                            Previous.Right = null;
+                        }
+                    }
+                    else if (Left == null && Right != null)
+                    {
+                        if (wentLeft)
+                        {
+                            Previous.Left = Right;
+                        }
+                        else
+                        {
+                            Previous.Right = Right;
+                        }
+                    }
+                    else if (Left != null && Right == null)
+                    {
+                        if (wentLeft)
+                        {
+                            Previous.Left = Left;
+                        }
+                        else
+                        {
+                            Previous.Right = Left;
+                        }
+                    }
+                    else
+                    {
+                        int temp;
+                        if (wentLeft)
+                        {
+                            temp = Right.Value;
+                            Right.Remove(temp, this);
+                            Value = temp;
+                        }
+                        else
+                        {
+                            temp = Left.Value;
+                            Left.Remove(temp, this);
+                            Value = temp;
+                        }
                     }
                 }
+                else if (val > Value)
+                {
+                    Right.Remove(val, this);
+                }
+                else
+                {
+                    Left.Remove(val, this);
+                }
             }
-            private bool IsLeaf()
+            public bool IsLeaf()
             {
                 if (Left == null && Right == null)
                 {
