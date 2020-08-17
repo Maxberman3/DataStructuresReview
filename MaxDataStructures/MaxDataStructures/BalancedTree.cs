c4442567ac8bd70665a2fc927daa2b50f4962eaa
Berman, Maxwell <maxwell.berman@emerson.com>
2020-08-17 12:13:49 -0400

    Started working on implementaton of balanced tree

---
+++ MaxDataStructures/MaxDataStructures/BalancedTree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace MaxDataStructures
+{
+    public class BalancedTree
+    {
+        private BalancedTreeNode Root { get; set; }
+        public int Balance
+        {
+            get
+            {
+                return Root.Balance;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return Root.Height;
+            }
+
+        }
+
+        public class BalancedTreeNode
+        {
+            public IComparable Value { get; set; }
+            public BalancedTreeNode Left { get; set; }
+            public BalancedTreeNode Right { get; set; }
+            public int Height
+            {
+                get
+                {
+                    if (Left == null && Right == null)
+                    {
+                        return 0;
+                    }
+                    else if (Left == null && Right != null)
+                    {
+                        return 1 + Right.Height;
+                    }
+                    else if (Left != null && Right == null)
+                    {
+                        return 1 + Left.Height;
+                    }
+                    else
+                    {
+                        return 1 + Math.Max(Left.Height, Right.Height);
+                    }
+                }
+            }
+            public int Balance
+            {
+                get
+                {
+                    if (Left != null && Right != null)
+                    {
+                        return Left.Height - Right.Height;
+                    }
+                    else if (Left == null && Right == null)
+                    {
+                        return 0;
+                    }
+                    else if (Right != null && Left == null)
+                    {
+                        return -Right.Height;
+                    }
+                    else if (Left != null && Right == null)
+                    {
+                        return Left.Height;
+                    }
+                    else
+                    {
+                        return Left.Height - Right.Height;
+                    }
+                }
+            }
+            public BalancedTreeNode(IComparable Value)
+            {
+                this.Value = Value;
+            }
+            public void Insert(IComparable newValue)
+            {
+                if (newValue.CompareTo(Value) <= 0)
+                {
+                    Left = new BalancedTreeNode(newValue);
+                }
+                else
+                {
+                    Right = new BalancedTreeNode(newValue);
+                }
+            }
+
+        }
+    }
+}
